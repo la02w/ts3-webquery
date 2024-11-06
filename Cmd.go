338d44ad5b82b3b2e2b59bbf13c822ee736bd9ac
la02w <1786075785@qq.com>
2024-11-06 14:56:04 +0800

    添加cmd和execcmd 方法

---
+++ Cmd.go
@@ -0,0 +1,30 @@
+package ts3_webquery
+
+import "encoding/json"
+
+func (c *Client) Cmd(cmd string) (*Response, error) {
+	url := c.WebQuery + "/1/" + cmd + "?api-key=" + c.APIKey
+	body, err := Get(url, c.TimeOut)
+	if err != nil {
+		return nil, err
+	}
+	var response Response
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		return nil, err
+	}
+	return &response, nil
+}
+func (c *Client) ExecCmd(cmd string, data map[string]string) (*Response, error) {
+	url := c.WebQuery + "/1/" + cmd + "?api-key=" + c.APIKey
+	body, err := Post(url, c.TimeOut, data)
+	if err != nil {
+		return nil, err
+	}
+	var response Response
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		return nil, err
+	}
+	return &response, nil
+}
