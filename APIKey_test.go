3888d74d9befda9e9a082ed7bd1b98a4cb5d077e
La02w <1786075785@qq.com>
2024-11-08 17:59:42 +0800

    添加apikey和测试方法

---
+++ APIKey_test.go
@@ -0,0 +1,56 @@
+package ts3_webquery
+
+import (
+	"encoding/json"
+	"os"
+	"testing"
+	"time"
+)
+
+func TestAPIKeyAdd(t *testing.T) {
+	c, _ := Login("http://127.0.0.1:10080", "BABf9LIMFyscvC4X68S4WBDlGIRO8wR2XupSTni", 5*time.Second)
+	data := map[string]string{
+		"scope": "read",
+	}
+	resp, err := c.APIKeyAdd(data)
+	if err != nil {
+		t.Log(err)
+	} else {
+		SaveToJSONFile(resp)
+	}
+}
+
+func TestAPIKeyDels(t *testing.T) {
+	// t.Log("del")
+	c, _ := Login("http://127.0.0.1:10080", "BABf9LIMFyscvC4X68S4WBDlGIRO8wR2XupSTni", 5*time.Second)
+	resp, err := c.APIKeyDel("22")
+	if err != nil {
+		t.Log(err)
+	} else {
+		SaveToJSONFile(resp)
+	}
+}
+
+func TestAPIKeyList(t *testing.T) {
+	c, _ := Login("http://127.0.0.1:10080", "BABf9LIMFyscvC4X68S4WBDlGIRO8wR2XupSTni", 5*time.Second)
+	data := map[string]string{
+		"-count": "",
+	}
+	resp, err := c.APIKeyList(data)
+	if err != nil {
+		t.Log(err)
+	} else {
+		SaveToJSONFile(resp)
+	}
+}
+
+func SaveToJSONFile(data interface{}) error {
+	// 将结构体序列化为JSON
+	jsonData, err := json.MarshalIndent(data, "", "    ")
+	if err != nil {
+		return err
+	}
+
+	// 将JSON数据写入文件
+	return os.WriteFile("test.json", jsonData, 0644)
+}
