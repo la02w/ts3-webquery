3b641d13c1eff0a5cce6e9de31beae79ea8ec652
La02w <1786075785@qq.com>
2024-11-11 14:03:37 +0800

    添加空方法，还没实现

--- Client.go
+++ Client.go
@@ -1,9 +1,134 @@
 package ts3_webquery
 
-import (
-	"encoding/json"
-)
+import "encoding/json"
 
+/*
+   clientaddperm               | assign permission to client
+   clientdbdelete              | delete client database properties
+   clientdbedit                | change client database properties
+   clientdbfind                | find client database ID by nickname or UID
+   clientdbinfo                | display client database properties
+   clientdblist                | list known client UIDs
+   clientdelperm               | remove permission from client
+   clientedit                  | change client properties
+   clientfind                  | find client by nickname
+   clientgetdbidfromuid        | find client database ID by UID
+   clientgetids                | find client IDs by UID
+   clientgetnamefromdbid       | find client nickname by database ID
+   clientgetnamefromuid        | find client nickname by UID
+   clientgetuidfromclid        | find client UID by client ID
+   clientinfo                  | display client properties
+   clientkick                  | kick a client
+   clientlist                  | list clients online on a virtual server
+   clientmove                  | move a client
+   clientpermlist              | list client specific permissions
+   clientpoke                  | poke a client
+   clientsetserverquerylogin   | set own login credentials
+   clientupdate                | set own properties
+*/
+
+// clientaddperm               | assign permission to client
+// Description:
+// Parameters:
+func (c *Client) ClientAddPerm() {}
+
+// clientdbdelete              | delete client database properties
+// Description:
+// Parameters:
+func (c *Client) ClientDBDelete() {}
+
+// clientdbedit                | change client database properties
+// Description:
+// Parameters:
+func (c *Client) ClientDBEdit() {}
+
+// clientdbfind                | find client database ID by nickname or UID
+// Description:
+// Parameters:
+func (c *Client) ClientDBFind() {}
+
+// clientdbinfo                | display client database properties
+// Description:
+// Parameters:
+func (c *Client) ClientDBInfo() {}
+
+// clientdblist                | list known client UIDs
+// Description:
+// Parameters:
+func (c *Client) ClientDBList() {}
+
+// clientdelperm               | remove permission from client
+// Description:
+// Parameters:
+func (c *Client) ClientDelPerm() {}
+
+// clientedit                  | change client properties
+// Description:
+// Parameters:
+func (c *Client) ClientEdit() {}
+
+// clientfind                  | find client by nickname
+// Description:
+// Parameters:
+func (c *Client) ClientFind() {}
+
+// clientgetdbidfromuid        | find client database ID by UID
+// Description:
+// Parameters:
+func (c *Client) ClientGetDBIDFormUID() {}
+
+// clientgetids                | find client IDs by UID
+// Description:
+// Parameters:
+func (c *Client) ClientGetIDS() {}
+
+// clientgetnamefromdbid       | find client nickname by database ID
+// Description:
+// Parameters:
+func (c *Client) ClientGetNameFormDBID() {}
+
+// clientgetnamefromuid        | find client nickname by UID
+// Description:
+// Parameters:
+func (c *Client) ClientGetNameFormUID() {}
+
+// clientgetuidfromclid        | find client UID by client ID
+// Description:
+// Parameters:
+func (c *Client) ClientGetUIDFromClID() {}
+
+// clientinfo                  | display client properties
+// Description:
+// Parameters:
+func (c *Client) ClientInfo() {}
+
+// clientkick                  | kick a client
+// Description:
+// Parameters:
+func (c *Client) ClientKick() {}
+
+//	clientlist                  | 'list clients online on a virtual server'
+//
+// Description:
+//
+//	Displays a list of clients online on a virtual server including their ID,
+//	nickname, status flags, etc. The output can be modified using several command
+//	options.
+//	Please note that the output will only contain clients which are currently in
+//	channels you are able to subscribe to.
+//
+// Parameters:
+//
+//	'-uid' : Reports the client unique identifier
+//	'-away' : Adds client away status and message
+//	'-voice' : Adds information on whether the user is talking, their mute/hardware status and if they are recording/priority speaker
+//	'-times' : Add idle, created and last connected time.
+//	'-groups' : Includes info on server and channel groups.
+//	'-info' : Adds client version and platform
+//	'-country' : Reports the client country code
+//	'-ip' : Adds the client IP, depending on the callers **PERMISSION_b_client_remoteaddress_view** permission
+//	'-icon' : Adds the client icon id
+//	'-badges' : Reports on the clients badges
 func (c *Client) ClientList(data map[string]string) (*Response, error) {
 	url := c.WebQuery + "/1/clientlist?api-key=" + c.APIKey
 	body, err := Post(url, c.TimeOut, data)
@@ -17,3 +142,28 @@ func (c *Client) ClientList(data map[string]string) (*Response, error) {
 	}
 	return &response, nil
 }
+
+// clientmove                  | move a client
+// Description:
+// Parameters:
+func (c *Client) ClientMove() {}
+
+// clientpermlist              | list client specific permissions
+// Description:
+// Parameters:
+func (c *Client) ClientPermList() {}
+
+// clientpoke                  | poke a client
+// Description:
+// Parameters:
+func (c *Client) ClientPoke() {}
+
+// clientsetserverquerylogin   | set own login credentials
+// Description:
+// Parameters:
+func (c *Client) ClientSetServerQueryLogin() {}
+
+// clientupdate                | set own properties
+// Description:
+// Parameters:
+func (c *Client) ClientUpdate() {}
--- Server.go
+++ Server.go
@@ -4,9 +4,157 @@ import (
 	"encoding/json"
 )
 
-// Displays detailed configuration information about the selected virtual server including unique ID, number of clients online, configuration, etc. For detailed information, see Virtual Server Properties.
+/*
+   servercreate                | create a virtual server
+   serverdelete                | delete a virtual server
+   serveredit                  | change virtual server properties
+   servergroupadd              | create a server group
+   servergroupaddclient        | add client to server group
+   servergroupaddperm          | assign permissions to server group
+   servergroupautoaddperm      | globally assign permissions to server groups
+   servergroupautodelperm      | globally remove permissions from server group
+   servergroupclientlist       | list clients in a server group
+   servergroupcopy             | create a copy of an existing server group
+   servergroupdel              | delete a server group
+   servergroupdelclient        | remove client from server group
+   servergroupdelperm          | remove permissions from server group
+   servergrouplist             | list server groups
+   servergrouppermlist         | list server group permissions
+   servergrouprename           | rename a server group
+   servergroupsbyclientid      | get all server groups of specified client
+   serveridgetbyport           | find database ID by virtual server port
+   serverinfo                  | display virtual server properties
+   serverlist                  | list virtual servers
+   servernotifyregister        | register for event notifications
+   servernotifyunregister      | unregister from event notifications
+   serverprocessstop           | shutdown server process
+   serverrequestconnectioninfo | display virtual server connection info
+   serversnapshotcreate        | create snapshot of a virtual server
+   serversnapshotdeploy        | deploy snapshot of a virtual server
+   serverstart                 | start a virtual server
+   serverstop                  | stop a virtual server
+   servertemppasswordadd       | create a new temporary server password
+   servertemppassworddel       | delete an existing temporary server password
+   servertemppasswordlist      | list all existing temporary server passwords
+*/
+
+//	servercreate                | 'create a virtual server'
 //
-// 显示关于所选虚拟服务器的详细配置信息，包括唯一 ID、在线客户端数量、配置等。有关详细信息，请参阅虚拟服务器属性。
+// Description:
+// Parameters:
+func (c *Client) ServerCreate() {}
+
+//	serverdelete                | 'delete a virtual server'
+//
+// Description:
+// Parameters:
+func (c *Client) ServerDelete() {}
+
+//	serveredit                  | 'change virtual server properties'
+//
+// Description:
+// Parameters:
+func (c *Client) ServerEdit() {}
+
+//	servergroupadd              | 'create a server group'
+//
+// Description:
+// Parameters:
+func (c *Client) ServerGroupAdd() {}
+
+//	servergroupaddclient        | 'add client to server group'
+//
+// Description:
+// Parameters:
+func (c *Client) ServerGroupAddClient() {}
+
+//	servergroupaddperm          | 'assign permissions to server group'
+//
+// Description:
+// Parameters:
+func (c *Client) ServerGroupAddPerm() {}
+
+//	servergroupautoaddperm      | 'globally assign permissions to server groups'
+//
+// Description:
+// Parameters:
+func (c *Client) ServerGroupAutoAddPerm() {}
+
+//	servergroupautodelperm      | 'globally remove permissions from server group'
+//
+// Description:
+// Parameters:
+func (c *Client) ServerGroupAutoDelPerm() {}
+
+//	servergroupclientlist       | 'list clients in a server group'
+//
+// Description:
+// Parameters:
+func (c *Client) ServerGroupClientList() {}
+
+//	servergroupcopy             | 'create a copy of an existing server group'
+//
+// Description:
+// Parameters:
+func (c *Client) ServerGroupCopy() {}
+
+//	servergroupdel              | 'delete a server group'
+//
+// Description:
+// Parameters:
+func (c *Client) ServerGroupDel() {}
+
+//	servergroupdelclient        | 'remove client from server group'
+//
+// Description:
+// Parameters:
+func (c *Client) ServerGroupDelClient() {}
+
+//	servergroupdelperm          | 'remove permissions from server group'
+//
+// Description:
+// Parameters:
+func (c *Client) ServerGroupDelPerm() {}
+
+//	servergrouplist             | 'list server groups'
+//
+// Description:
+// Parameters:
+func (c *Client) ServerGroupList() {}
+
+//	servergrouppermlist         | 'list server group permissions'
+//
+// Description:
+// Parameters:
+func (c *Client) ServerGroupPermList() {}
+
+//	servergrouprename           | 'rename a server group'
+//
+// Description:
+// Parameters:
+func (c *Client) ServerGroupRename() {}
+
+//	servergroupsbyclientid      | 'get all server groups of specified client'
+//
+// Description:
+// Parameters:
+func (c *Client) ServerGroupByClientID() {}
+
+//	serveridgetbyport           | 'find database ID by virtual server port'
+//
+// Description:
+// Parameters:
+func (c *Client) ServerIDGetByPort() {}
+
+//	serverinfo                  | 'display virtual server properties'
+//
+// Description:
+//
+//	Displays detailed configuration information about the selected virtual server
+//	including unique ID, number of clients online, configuration, etc.
+//	For detailed information, see Virtual Server Properties.
+//
+// Parameters:
 func (c *Client) ServerInfo() (*Response, error) {
 	url := c.WebQuery + "/1/serverinfo?api-key=" + c.APIKey
 	body, err := Get(url, c.TimeOut)
@@ -20,3 +168,75 @@ func (c *Client) ServerInfo() (*Response, error) {
 	}
 	return &response, nil
 }
+
+//	serverlist                  | 'list virtual servers'
+//
+// Description:
+// Parameters:
+func (c *Client) ServerList() {}
+
+//	servernotifyregister        | 'register for event notifications'
+//
+// Description:
+// Parameters:
+func (c *Client) ServerNotifyRegister() {}
+
+//	servernotifyunregister      | 'unregister from event notifications'
+//
+// Description:
+// Parameters:
+func (c *Client) ServerNotifyUnregister() {}
+
+//	serverprocessstop           | 'shutdown server process'
+//
+// Description:
+// Parameters:
+func (c *Client) ServerProcessTop() {}
+
+//	serverrequestconnectioninfo | 'display virtual server connection info'
+//
+// Description:
+// Parameters:
+func (c *Client) ServerRequestConnectionInfo() {}
+
+//	serversnapshotcreate        | 'create snapshot of a virtual server'
+//
+// Description:
+// Parameters:
+func (c *Client) ServerSnapshotCreate() {}
+
+//	serversnapshotdeploy        | 'deploy snapshot of a virtual server'
+//
+// Description:
+// Parameters:
+func (c *Client) ServerSnapshotDeploy() {}
+
+//	serverstart                 | 'start a virtual server'
+//
+// Description:
+// Parameters:
+func (c *Client) ServerStart() {}
+
+//	serverstop                  | 'stop a virtual server'
+//
+// Description:
+// Parameters:
+func (c *Client) SaverStop() {}
+
+//	servertemppasswordadd       | 'create a new temporary server password'
+//
+// Description:
+// Parameters:
+func (c *Client) ServerTempPasswordAdd() {}
+
+//	servertemppassworddel       | 'delete an existing temporary server password'
+//
+// Description:
+// Parameters:
+func (c *Client) ServerTempPasswordDel() {}
+
+//	servertemppasswordlist      | 'list all existing temporary server passwords'
+//
+// Description:
+// Parameters:
+func (c *Client) ServerTempPasswordList() {}
